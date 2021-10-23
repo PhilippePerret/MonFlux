cd06b2fb464456d0301711c7b21ec3b2c42ee5eb
Philippe Perret <philippe.perret@yahoo.fr>
2021-10-23 11:55:10 +0200

    Suppression des messages de test

--- js/on_ready.js
+++ js/on_ready.js
@@ -4,6 +4,4 @@ $(document).ready(e => {
 
   TaskContainer.all.forEach(cont => cont.prepare())
   Tasks.load_and_display() 
-  message("Je suis prête")
-  setTimeout(message.bind(null,"Je suis toujours prête"), 2000)
 })
